e84599f037f6de5a1fb650e76242fbbcbf33145c
guysolamour <rolandassale@yandex.com>
2017-10-24 00:06:15 +0000

    fix the menu of the top

--- js-src/app.js
+++ js-src/app.js
@@ -1,1 +1,49 @@
-console.info("Guysolamour");
+// (function() {
+//     var scrollY = function() {
+//         var supportPageOffset = window.pageXOffset !== undefined;
+//         var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+//         // var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
+//         return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+//     }
+
+//     var elements = document.querySelectorAll('[data-sticky]');
+//     for (var i = 0; i < elements.length; i++) {
+//         (function(element) {
+//             var rect = element.getBoundingClientRect();
+//             console.log(rect);
+//             var offset = parseInt(element.getAttribute('data-offset') || 0, 10);
+//             var top = rect.top + scrollY();
+//             var fake = document.createElement('div');
+//             fake.style.width = rect.width + 'px';
+//             fake.style.height = rect.height + 'px';
+//             //Functions
+//             var onScroll = function() {
+//                 var hasScroll = element.classList.contains('fixed');
+//                 if (scrollY() > top - offset && !hasScroll) {
+//                     element.classList.add('fixed');
+//                     element.style.top = offset + 'px';
+//                     element.style.width = rect.width + 'px';
+//                     element.parentNode.insertBefore(fake, element);
+//                 } else if (scrollY() < top - offset && hasScroll) {
+//                     element.classList.remove('fixed');
+//                     element.parentNode.removeChild(fake);
+//                 }
+//             }
+//             var onResize = function() {
+//                     element.style.width = 'auto';
+//                     element.classList.remove('fixed');
+//                     fake.style.display = 'none';
+//                     rect = element.getBoundingClientRect();
+//                     top = rect.top + scrollY();
+//                     fake.style.width = rect.width + 'px';
+//                     fake.style.height = rect.height + 'px';
+//                     fake.style.display = 'block';
+//                     onScroll();
+//                 }
+//                 // Listeners
+//             window.addEventListener('scroll', onScroll);
+//             window.addEventListener('resize', onResize);
+
+//         })(elements[i]);
+//     }
+// })();
